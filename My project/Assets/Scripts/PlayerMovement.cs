49de1b1e6b875beabc35ff3d280e3e11f92312c8
Anish Labroo <102484252+ALabroo18@users.noreply.github.com>
2025-02-13 17:17:10 -0500

    Added Walking and Jumping

--- My project/Assets/Scripts/PlayerMovement.cs
+++ My project/Assets/Scripts/PlayerMovement.cs
@@ -3,11 +3,12 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     float horizontalMovement, verticalMovement;
-    public Rigidbody2D rb2d;
+    private Rigidbody2D rb2d;
+    private float jumpPower = 12f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        rb2d = this.GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -15,8 +16,12 @@ public class PlayerMovement : MonoBehaviour
     {
         horizontalMovement = Input.GetAxisRaw("Horizontal");
         verticalMovement = Input.GetAxis("Vertical");
+        if (Input.GetButtonDown("Jump"))
+        {
+            rb2d.velocity = new Vector2(rb2d.velocity.x, jumpPower);
+        }
 
-        
+
 
     }
     private void FixedUpdate()
@@ -25,6 +30,8 @@ public class PlayerMovement : MonoBehaviour
         this.transform.position = new Vector3(0, verticalMovement * 5f, 0) * Time.deltaTime;*/
 
         rb2d.velocity = new Vector2(horizontalMovement * 5.0f, rb2d.velocity.y);
+
+        
 
     }
 }
